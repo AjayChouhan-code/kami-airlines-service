aa7a27d8836f6b3e5cf0d49d05e75a6a72c2a05e
Ajay Chouhan <ajay0312chouhan@gmail.com>
2025-01-08 20:07:42 +0530

    Stage: Add abstract base models, and soft delete model in db.

---
+++ db/base_models.py
@@ -0,0 +1,59 @@
+import uuid
+
+from django.db import models
+
+from django_mysql.models import Bit1BooleanField
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    # ID settings
+    id = models.BigAutoField(primary_key=True, unique=True)
+    uuid = models.BigIntegerField(editable=False, unique=True)
+
+    # Create and update triggers
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    @classmethod
+    def get_new_uuid(cls):
+        return uuid.uuid4().int >> 75
+
+    def save(self, **kwargs):
+        if not self.uuid:
+            self.public_id = self.get_new_uuid()
+
+        super(BaseModel, self).save(**kwargs)
+
+
+class BooleanField(Bit1BooleanField, models.BooleanField):
+    def db_type(self, connection):
+        if connection.settings_dict["ENGINE"] == "django.db.backends.mysql":
+            return super(Bit1BooleanField, self).db_type(connection)
+        else:
+            return super(BooleanField, self).db_type(connection)
+
+
+# Soft Delete #
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = BooleanField(default=False, null=True)
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+
+    class Meta:
+        abstract = True
